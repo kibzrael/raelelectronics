dd47489e40b0c27d2efbbb02a2f3a3c0374f34e0
kibzrael <kibraphael7@gmail.com>
2024-09-27 15:15:53 +0300

    save laptop to database

--- catalog/data/schema.go
+++ catalog/data/schema.go
@@ -1,10 +1,13 @@
 package data
 
 import (
+	"context"
 	"encoding/json"
 	"strconv"
 	"strings"
-	"time"
+
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
 )
 
 const Schema = `
@@ -15,7 +18,7 @@ const Schema = `
 		brand				varchar(255) NOT NULL,
 		thumbnail			varchar(255) NOT NULL,
 		link				varchar(255) NULL,
-		launched			TIMESTAMP NULL,
+		launched			Date NULL,
 		price_min			float NOT NULL,
 		price_max			float NOT NULL,
 
@@ -24,7 +27,7 @@ const Schema = `
 
 	CREATE TABLE IF NOT EXISTS cpus (
 		uid					varchar(80) PRIMARY KEY,
-		laptop				varchar(80) REFERENCES laptops,
+		laptop				varchar(80) REFERENCES laptops ON DELETE CASCADE,
 		model				varchar(255) NOT NULL,
 		architecture		varchar(255) NULL,
 		lithography			integer NULL,
@@ -41,7 +44,7 @@ const Schema = `
 
 	CREATE TABLE IF NOT EXISTS gpus (
 		uid					varchar(80) PRIMARY KEY,
-		laptop				varchar(80) REFERENCES laptops,
+		laptop				varchar(80) REFERENCES laptops ON DELETE CASCADE,
 		producer			varchar(255) NULL,
 		model				varchar(255) NOT NULL,
 		architecture		varchar(255) NULL,
@@ -59,7 +62,7 @@ const Schema = `
 
 	CREATE TABLE IF NOT EXISTS displays (
 		uid					varchar(80) PRIMARY KEY,
-		laptop				varchar(80) REFERENCES laptops,
+		laptop				varchar(80) REFERENCES laptops ON DELETE CASCADE,
 		size				float NOT NULL,
 		h_resolution		integer NOT NULL,
 		v_resolution		integer NOT NULL,
@@ -73,7 +76,7 @@ const Schema = `
 
 	CREATE TABLE IF NOT EXISTS memorys (
 		uid					varchar(80) PRIMARY KEY,
-		laptop				varchar(80) REFERENCES laptops,
+		laptop				varchar(80) REFERENCES laptops ON DELETE CASCADE,
 		size				integer NOT NULL,
 		speed				float NULL,
 		type				varchar(255) NULL,
@@ -83,7 +86,7 @@ const Schema = `
 
 	CREATE TABLE IF NOT EXISTS storages (
 		uid					varchar(80) PRIMARY KEY,
-		laptop				varchar(80) REFERENCES laptops,
+		laptop				varchar(80) REFERENCES laptops ON DELETE CASCADE,
 		capacity			integer NOT NULL,
 		speed				integer NULL,
 		model				varchar(255) NULL,
@@ -93,7 +96,7 @@ const Schema = `
 
 	CREATE TABLE IF NOT EXISTS wireless_cards (
 		uid					varchar(80) PRIMARY KEY,
-		laptop				varchar(80) REFERENCES laptops,
+		laptop				varchar(80) REFERENCES laptops ON DELETE CASCADE,
 		speed				integer NULL,
 		model				varchar(255) NULL,
 		other_info			varchar(255) NULL,
@@ -103,7 +106,7 @@ const Schema = `
 
 	CREATE TABLE IF NOT EXISTS motherboards (
 		uid					varchar(80) PRIMARY KEY,
-		laptop				varchar(80) REFERENCES laptops,
+		laptop				varchar(80) REFERENCES laptops ON DELETE CASCADE,
 		ram					varchar(255) NULL,
 		storage				varchar(255) NULL,
 
@@ -112,7 +115,7 @@ const Schema = `
 
 	CREATE TABLE IF NOT EXISTS chassis (
 		uid					varchar(80) PRIMARY KEY,
-		laptop				varchar(80) REFERENCES laptops,
+		laptop				varchar(80) REFERENCES laptops ON DELETE CASCADE,
 		height				float NOT NULL,
 		depth				float NOT NULL,
 		width				float NOT NULL,
@@ -126,8 +129,7 @@ const Schema = `
 
 	CREATE TABLE IF NOT EXISTS ports (
 		uid					varchar(80) PRIMARY KEY,
-		laptop				varchar(80) REFERENCES laptops,
-		chassis				varchar(80) REFERENCES chassis,
+		chassis				varchar(80) REFERENCES chassis ON DELETE CASCADE,
 		count				integer NULL,
 		type				varchar(255) NULL,
 
@@ -136,7 +138,7 @@ const Schema = `
 
 	CREATE TABLE IF NOT EXISTS batteries (
 		uid					varchar(80) PRIMARY KEY,
-		laptop				varchar(80) REFERENCES laptops,
+		laptop				varchar(80) REFERENCES laptops ON DELETE CASCADE,
 		capacity			float NOT NULL,
 		type				varchar(255) NULL,
 		life				float NULL,
@@ -147,15 +149,20 @@ const Schema = `
 
 	CREATE TABLE IF NOT EXISTS images (
 		uid					varchar(80) PRIMARY KEY,
-		laptop				varchar(80) REFERENCES laptops,
+		laptop				varchar(80) REFERENCES laptops ON DELETE CASCADE,
 		url					varchar(255) NOT NULL,
 
 		created_at			TIMESTAMP NOT NULL DEFAULT NOW()
 	);
 `
 
+type LaptopComponent interface {
+	Save(tx *sqlx.Tx, l *Laptop) error
+}
+
 type CPU struct {
 	Uid             string
+	Laptop          string
 	Model           string  `json:"model"`
 	Architecture    string  `json:"prod"`
 	Lithography     uint64  `json:"lithography,string"`
@@ -168,8 +175,19 @@ type CPU struct {
 	OtherInfo       string  `json:"other_info" db:"other_info"`
 }
 
+func (c *CPU) Save(tx *sqlx.Tx, l *Laptop) error {
+	c.Uid = uuid.New().String()
+	c.Laptop = l.Uid
+	_, err := tx.NamedExec(`
+		INSERT INTO cpus (uid, laptop, model, architecture, lithography, cache, base_speed, boost_speed, cores, tdp, integrated_video, other_info) 
+		VALUES (:uid, :laptop, :model, :architecture, :lithography, :cache, :base_speed, :boost_speed, :cores, :tdp, :integrated_video, :other_info)
+	`, c)
+	return err
+}
+
 type GPU struct {
 	Uid          string
+	Laptop       string
 	Producer     string  `json:"prod"`
 	Model        string  `json:"model"`
 	Architecture string  `json:"architecture"`
@@ -183,8 +201,19 @@ type GPU struct {
 	OtherInfo    string  `json:"other_info" db:"other_info"`
 }
 
+func (g *GPU) Save(tx *sqlx.Tx, l *Laptop) error {
+	g.Uid = uuid.New().String()
+	g.Laptop = l.Uid
+	_, err := tx.NamedExec(`
+		INSERT INTO gpus (uid, laptop, model, architecture, lithography, producer, base_speed, boost_speed, memory_size, tdp, memory_speed, memory_type, other_info) 
+		VALUES (:uid, :laptop, :model, :architecture, :lithography, :producer, :base_speed, :boost_speed, :memory_size, :tdp, :memory_speed, :memory_type, :other_info)
+	`, g)
+	return err
+}
+
 type Display struct {
 	Uid         string
+	Laptop      string
 	Size        float64 `json:"size,string"`
 	HResolution int64   `json:"horizontal_resolution,string" db:"h_resolution"`
 	VResolution int64   `json:"vertical_resolution,string" db:"v_resolution"`
@@ -194,41 +223,107 @@ type Display struct {
 	OtherInfo   string `json:"other_info" db:"other_info"`
 }
 
+func (d *Display) Save(tx *sqlx.Tx, l *Laptop) error {
+	d.Uid = uuid.New().String()
+	d.Laptop = l.Uid
+	_, err := tx.NamedExec(`
+		INSERT INTO displays (uid, laptop, size, h_resolution, v_resolution, type, srgb, touchscreen, other_info) 
+		VALUES (:uid, :laptop, :size, :h_resolution, :v_resolution, :type, :srgb, :touchscreen, :other_info)
+	`, d)
+	return err
+}
+
 type Memory struct {
-	Uid   string
-	Size  int64   `json:"size,string"`
-	Speed float64 `json:"speed,string"`
-	Type  string  `json:"type"`
+	Uid    string
+	Laptop string
+	Size   int64   `json:"size,string"`
+	Speed  float64 `json:"speed,string"`
+	Type   string  `json:"type"`
+}
+
+func (m *Memory) Save(tx *sqlx.Tx, l *Laptop) error {
+	m.Uid = uuid.New().String()
+	m.Laptop = l.Uid
+	_, err := tx.NamedExec(`
+		INSERT INTO memorys (uid, laptop, size, speed, type) 
+		VALUES (:uid, :laptop, :size, :speed, :type)
+	`, m)
+	return err
 }
 
 type Storage struct {
 	Uid      string
+	Laptop   string
 	Capacity int64  `json:"cap,string"`
 	Speed    int64  `json:"read_speed,string"`
 	Model    string `json:"model"`
 }
 
+func (s *Storage) Save(tx *sqlx.Tx, l *Laptop) error {
+	s.Uid = uuid.New().String()
+	s.Laptop = l.Uid
+	_, err := tx.NamedExec(`
+		INSERT INTO storages (uid, laptop, capacity, speed, model) 
+		VALUES (:uid, :laptop, :capacity, :speed, :model)
+	`, s)
+	return err
+}
+
 type WirelessCard struct {
 	Uid       string
+	Laptop    string
 	Model     string `json:"model"`
 	Speed     int64  `json:"speed,string"`
 	OtherInfo string `json:"other_info" db:"other_info"`
 }
 
+func (w *WirelessCard) Save(tx *sqlx.Tx, l *Laptop) error {
+	w.Uid = uuid.New().String()
+	w.Laptop = l.Uid
+	_, err := tx.NamedExec(`
+		INSERT INTO wireless_cards (uid, laptop, model, speed, other_info) 
+		VALUES (:uid, :laptop, :model, :speed, :other_info)
+	`, w)
+	return err
+}
+
 type Motherboard struct {
 	Uid     string
+	Laptop  string
 	Ram     string `json:"ram_slots"`
 	Storage string `json:"storage_slots"`
 }
 
+func (m *Motherboard) Save(tx *sqlx.Tx, l *Laptop) error {
+	m.Uid = uuid.New().String()
+	m.Laptop = l.Uid
+	_, err := tx.NamedExec(`
+		INSERT INTO motherboards (uid, laptop, ram, storage) 
+		VALUES (:uid, :laptop, :ram, :storage)
+	`, m)
+	return err
+}
+
 type Port struct {
-	Uid   string
-	Count uint64
-	Type  string
+	Uid     string
+	Chassis string
+	Count   uint64
+	Type    string
+}
+
+func (p *Port) Save(tx *sqlx.Tx, c *Chassis) error {
+	p.Uid = uuid.New().String()
+	p.Chassis = c.Uid
+	_, err := tx.NamedExec(`
+		INSERT INTO ports (uid, chassis, count, type) 
+		VALUES (:uid, :chassis, :count, :type)
+	`, p)
+	return err
 }
 
 type Chassis struct {
 	Uid       string
+	Laptop    string
 	Height    float64 `json:"height_cm,string"`
 	Depth     float64 `json:"depth_cm,string"`
 	Width     float64 `json:"width_cm,string"`
@@ -239,12 +334,36 @@ type Chassis struct {
 	OtherInfo string  `json:"other_info" db:"other_info"`
 }
 
+func (c *Chassis) Save(tx *sqlx.Tx, l *Laptop) error {
+	c.Uid = uuid.New().String()
+	c.Laptop = l.Uid
+	_, err := tx.Exec(`
+		INSERT INTO chassis (uid, laptop, height, depth, width, weight, colors, webcam, other_info) 
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+	`, c.Uid, c.Laptop, c.Height, c.Depth, c.Width, c.Weight, strings.Join(c.Colors, ","), c.Webcam, c.OtherInfo)
+	for _, port := range c.ports {
+		err = port.Save(tx, c)
+	}
+	return err
+}
+
 type Battery struct {
 	Uid       string
+	Laptop    string
 	Capacity  float64 `json:"capacity,string"`
 	Type      string  `json:"cell_type"`
 	Life      float64
 	OtherInfo string `json:"other_info" db:"other_info"`
+}
+
+func (b *Battery) Save(tx *sqlx.Tx, l *Laptop) error {
+	b.Uid = uuid.New().String()
+	b.Laptop = l.Uid
+	_, err := tx.NamedExec(`
+		INSERT INTO batteries (uid, laptop, capacity, type, life, other_info) 
+		VALUES (:uid, :laptop, :capacity, :type, :life, :other_info)
+	`, b)
+	return err
 }
 
 type Laptop struct {
@@ -255,7 +374,7 @@ type Laptop struct {
 	Thumbnail    string
 	Images       []string
 	Link         string
-	Launched     time.Time
+	Launched     string
 	PriceMin     float64 `db:"price_min"`
 	PriceMax     float64 `db:"price_max"`
 	CPU          []CPU
@@ -269,6 +388,65 @@ type Laptop struct {
 	Battery      []Battery
 }
 
+func (l *Laptop) Save(ctx context.Context) error {
+	db := ctx.Value(DB_CONTEXT).(*sqlx.DB)
+
+	tx := db.MustBegin()
+	l.Uid = uuid.New().String()
+	_, err := tx.NamedExec(`
+		INSERT INTO laptops (uid, noteb_id, name, brand, thumbnail, link, launched, price_min, price_max) 
+		VALUES (:uid, :noteb_id, :name, :brand, :thumbnail, :link, :launched, :price_min, :price_max)
+	`, l)
+	for _, img := range l.Images {
+		_, err = tx.Exec(`
+			INSERT INTO images (uid, laptop, url) VALUES ($1, $2, $3)
+		`, uuid.New().String(), l.Uid, img)
+	}
+
+	// fields := []any{l.CPU, l.GPU, l.Display, l.Memory, l.Storage, l.WirelessCard, l.Motherboard, l.Chassis, l.Battery}
+	// for _, f := range fields {
+	// 	components := f.([]LaptopComponent)
+	// 	for _, c := range components {
+	// 		err = c.Save(tx, l)
+	// 	}
+	// }
+
+	for _, cpu := range l.CPU {
+		err = cpu.Save(tx, l)
+	}
+	for _, gpu := range l.GPU {
+		err = gpu.Save(tx, l)
+	}
+	for _, display := range l.Display {
+		err = display.Save(tx, l)
+	}
+	for _, memory := range l.Memory {
+		err = memory.Save(tx, l)
+	}
+	for _, storage := range l.Storage {
+		err = storage.Save(tx, l)
+	}
+	for _, wireless_card := range l.WirelessCard {
+		err = wireless_card.Save(tx, l)
+	}
+	for _, motherboard := range l.Motherboard {
+		err = motherboard.Save(tx, l)
+	}
+	for _, chassis := range l.Chassis {
+		err = chassis.Save(tx, l)
+	}
+	for _, battery := range l.Battery {
+		err = battery.Save(tx, l)
+	}
+
+	if err != nil {
+		_ = tx.Rollback()
+		return err
+	}
+	err = tx.Commit()
+	return err
+}
+
 func (l *Laptop) Serialize(data map[string]interface{}) {
 	modelInfoList := data["model_info"].([]interface{})
 	modelInfo := modelInfoList[0].(map[string]interface{})
@@ -279,7 +457,7 @@ func (l *Laptop) Serialize(data map[string]interface{}) {
 	moreResources := data["model_resources"].(map[string]interface{})
 	l.Thumbnail = moreResources["thumbnail"].(string)
 	l.Link = moreResources["official_link"].(string)
-	l.Launched, _ = time.Parse("2006-01-02", moreResources["launch_date"].(string))
+	l.Launched, _ = moreResources["launch_date"].(string)
 	l.PriceMin, _ = strconv.ParseFloat(data["config_price_min"].(string), 64)
 	l.PriceMax, _ = strconv.ParseFloat(data["config_price_max"].(string), 64)
 	for key, val := range moreResources {
--- catalog/go.mod
+++ catalog/go.mod
@@ -3,6 +3,7 @@ module github.com/kibzrael/raelelectronics/catalog
 go 1.23.0
 
 require (
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 )
